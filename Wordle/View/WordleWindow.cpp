0b3c21e0180087ba5062e237678e39e78f3db72a
Steven-Carriger <scarrig1@my.westga.edu>
2022-04-22 00:15:53 -0400

    Changed the size of the window to be more accommodating to the UI.
    Implemented a basic keyboard programatically that is a collection of buttons that all use the same callback.

--- Wordle/View/WordleWindow.cpp
+++ Wordle/View/WordleWindow.cpp
@@ -1,11 +1,62 @@
 #include "WordleWindow.h"
 
+#define NUMBER_OF_KEYS 28
+#define KEYBOARD_X 90
+#define KEYBOARD_Y 450
+#define BUTTON_WIDTH 50
+#define BUTTON_HEIGHT 50
+#define X_OFFSET 25
+#define MAX_FIRST_ROW 10
+#define MAX_OTHER_ROWS 9
+#define BUTTON_PADDING 55
+#define EMPTY_VALUE 0
 namespace view
 {
 
 WordleWindow::WordleWindow(int width, int height, const char* title) : Fl_Window(width, height, title)
 {
-    //ctor
+    begin();
+    this->keyLabels = {"Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Enter","Z","X","C","V","B","N","M","Undo"};
+    this->createTheKeyboard();
+    end();
+}
+
+void WordleWindow::createTheKeyboard()
+{
+    this->keyButtons = vector<Fl_Button*>();
+    this->addKeysToTheKeyboard();
+}
+
+void WordleWindow::addKeysToTheKeyboard()
+{
+    int x = KEYBOARD_X;
+    int y = KEYBOARD_Y;
+    int rowCount = EMPTY_VALUE;
+    bool isFirstRow = true;
+    for (int i = EMPTY_VALUE; i < NUMBER_OF_KEYS; i++)
+    {
+        Fl_Button* newButton = new Fl_Button(x + BUTTON_PADDING * rowCount, y, BUTTON_WIDTH, BUTTON_HEIGHT, this->keyLabels[i]);
+        newButton->callback(cbKeyPressed);
+        this->keyButtons.push_back(newButton);
+        rowCount++;
+
+        if (rowCount == MAX_FIRST_ROW && isFirstRow) {
+            x = KEYBOARD_X + X_OFFSET;
+            rowCount = EMPTY_VALUE;
+            y += BUTTON_PADDING;
+            isFirstRow = false;
+        }
+        if (rowCount == MAX_OTHER_ROWS && !isFirstRow) {
+            x = KEYBOARD_X + X_OFFSET;
+            rowCount = EMPTY_VALUE;
+            y += BUTTON_PADDING;
+        }
+    }
+}
+
+void WordleWindow::cbKeyPressed(Fl_Widget* widget)
+{
+    cout << "button: " << widget->label() << " pressed." << endl;
 }
 
 WordleWindow::~WordleWindow()
--- Wordle/main.cpp
+++ Wordle/main.cpp
@@ -7,7 +7,7 @@ using namespace view;
 
 int main (int argc, char ** argv)
 {
-  WordleWindow window(500, 500, "Wordle by Schmidt & Carriger");
+  WordleWindow window(750, 750, "Wordle by Schmidt & Carriger");
   window.show ();
   return(Fl::run());
 }
